88f6f3a442c5e4fb9a77dd7955171d5dd194e190
MuhammadFarhanAkram <farhankramryk@gmail.com>
2024-10-22 15:58:22 +0500

    First commit

---
+++ AssessmentNO3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AssessmentNO3
+//
+//  Created by Muhammad  Farhan Akram on 22/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
